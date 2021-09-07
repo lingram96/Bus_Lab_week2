272ad42d1f04b01b8160ea96f8b94c6b976590ac
Lewis Ingram <lewisingram@Lewiss-MacBook-Pro-2.local>
2021-09-07 18:45:37 +0100

    first commit

---
+++ src/bus.py
@@ -0,0 +1,31 @@
+class Bus:
+
+    def __init__(self, route_number, destination):
+        self.route_number = route_number
+        self.destination = destination
+        self.passengers = []
+
+    def drive(self):
+        return "Brum brum"
+
+    def passenger_count(self):
+        return len(self.passengers)
+    
+    def pick_up(self, person):
+        self.passengers.append(person)
+
+    def drop_off(self, person):
+        self.passengers.remove(person)
+
+    def empty(self):
+        self.passengers = []
+
+    def pick_up_from_stop(self, bus_stop):
+        for person in bus_stop.queue:
+            self.pick_up(person)
+        print(len(bus_stop.queue))
+        bus_stop.clear()
+        print(len(bus_stop.queue))
+
+
+
